86a99bae64235b7674018dd5205da727cfbff8a2
John Moshakis <john@moshakis.com>
2021-12-26 00:37:10 -0500

    MapKit extension

---
+++ Moshine.UI.MapKit/MKMapViewExtensions.pas
@@ -0,0 +1,23 @@
+﻿namespace Moshine.UI.MapKit;
+
+uses
+  MapKit;
+
+type
+  MKMapViewExtensions = public extension class(MKMapView)
+  public
+
+    method centerToLocation(location:CLLocationCoordinate2D);
+    begin
+      var regionRadius:CLLocationDistance := 1000;
+
+      var coordinateRegion := MKCoordinateRegionMakeWithDistance(
+        location, regionRadius, regionRadius);
+
+      self.setRegion(coordinateRegion) animated(true);
+
+    end;
+
+  end;
+
+end.
